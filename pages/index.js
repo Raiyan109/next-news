1df2de0760739cec8472b78ab0d4f0f87be5657e
Raiyan109 <kraiyan109@gmail.com>
2022-09-26 18:35:52 +0600

    home page default style deleted and custom style added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      Hello world
+      <div className='page-container'>
+        <div className={styles.main}>
+          <h1>Next.js News App</h1>
+          <h3>Your one stop shop for the latest news articles</h3>
+        </div>
+      </div>
     </>
   )
 }
