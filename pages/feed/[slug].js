cd3ff95fddc60466ea250809ec65baba45de2564
Raiyan109 <kraiyan109@gmail.com>
2022-09-27 19:51:20 +0600

    feed end

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,11 +1,13 @@
 import styles from '../../styles/Feed.module.css'
 import { useRouter } from 'next/router'
+import { Toolbar } from '../../components/toolbar'
 
 
 export const Feed = ({ pageNumber, articles }) => {
     const router = useRouter()
     return (
         <div className='page-container'>
+            <Toolbar />
             <div className={styles.main}>
                 {articles.map((article, index) => (
                     <div key={index} className={styles.post}>
@@ -25,6 +27,18 @@ export const Feed = ({ pageNumber, articles }) => {
                     }}
                     className={pageNumber === 1 ? styles.disabled : styles.active}>
                     Previous Page
+                </div>
+
+                <div>#{pageNumber}</div>
+
+                <div
+                    onClick={() => {
+                        if (pageNumber < 5) {
+                            router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0))
+                        }
+                    }}
+                    className={pageNumber === 5 ? styles.disabled : styles.active}>
+                    Next Page
                 </div>
             </div>
         </div>
