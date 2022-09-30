8699236ea62ea413627569f178b8d7975d6c6c94
Raiyan109 <kraiyan109@gmail.com>
2022-09-30 22:17:04 +0600

    banner

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Footer } from '../components/Footer'
 import { Toolbar } from '../components/toolbar'
 import styles from '../styles/Home.module.css'
+import bg from '../public/images/pexels-gabriel-peter-696644.jpg'
 
 export default function Home() {
   return (
@@ -9,7 +10,13 @@ export default function Home() {
       <div className='page-container'>
 
         <Toolbar />
-        <div className={styles.main}>
+        <div className={styles.main}
+          style={{
+            backgroundImage: `url(${bg.src})`,
+            width: '100%',
+            height: '100%',
+          }}
+        >
           <h1>Next.js News App</h1>
           <h3>Your one stop shop for the latest news articles</h3>
         </div>
